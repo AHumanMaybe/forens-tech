0b8d35fe6a5da6fd3a8dfb238d7ddd39299c3cdf
jlurnn <pcgamerdude17+github@gmail.com>
2025-04-13 00:19:59 -0400

    finish the struct for Exec

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, Request, APIRouter, Response, File, UploadFile, Form
+from pydantic import BaseModel
+import secrets
+import hashlib
+import time
+import json
+import os
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Exec(BaseModel):
+    dumpName: str
+    osInfo: bool
+    registry: bool
+    cmds: bool
+    procList: bool
+    netConn: bool
+    yara: bool
+
+
+@app.post("/request")
+async def root(response: Response, exec: Exec):
+    
+    
+
+    return {"message": "fail!"}
